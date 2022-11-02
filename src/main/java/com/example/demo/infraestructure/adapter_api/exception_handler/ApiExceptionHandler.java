5afcb3b206886083d7d7c44a8710e760e80d43dc
alejandrorsanchez <a.rodriguezsanchez15@gmail.com>
2022-11-02 17:53:34 +0100

    feat: Bad Request included in handler

--- src/main/java/com/example/demo/infraestructure/adapter_api/exception_handler/ApiExceptionHandler.java
+++ src/main/java/com/example/demo/infraestructure/adapter_api/exception_handler/ApiExceptionHandler.java
@@ -2,10 +2,15 @@ package com.example.demo.infraestructure.adapter_api.exception_handler;
 
 import com.example.demo.domain.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
@@ -16,6 +21,19 @@ public class ApiExceptionHandler {
     })
     @ResponseBody
     public ErrorMessage notFoundRequest(Exception exception) {
+        return new ErrorMessage(exception);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({
+            MethodArgumentTypeMismatchException.class,
+            WebExchangeBindException.class,
+            HttpMessageNotReadableException.class,
+            HttpRequestMethodNotSupportedException.class,
+            MethodArgumentNotValidException.class
+    })
+    @ResponseBody
+    public ErrorMessage badRequest(Exception exception) {
         return new ErrorMessage(exception);
     }
 
